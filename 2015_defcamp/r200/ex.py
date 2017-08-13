24ea5988a5fbaf8f0b08e1627aa14c98c9e5434d
DelspoN <delsponn@gmail.com>
2017-08-13 16:12:50 +0900

    r200 added

---
+++ 2015_defcamp/r200/ex.py
@@ -0,0 +1,5 @@
+import angr
+p = angr.Project("r200", load_options={'auto_load_libs': False})
+ex = p.surveyors.Explorer(find=(0x400936, ), avoid=(0x400947,), enable_veritesting=True)
+ex.run()
+print ex.found[0].state.posix.dumps(0)
